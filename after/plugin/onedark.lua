22b705137d8cf369793d2578ea00b32da01036fe
Timothée Keller <kellertimothee@gmail.com>
2023-09-03 00:13:37 +0200

    Port to lazy.nvim package manager.

---
+++ after/plugin/onedark.lua
@@ -0,0 +1,16 @@
+-- OneDark Setup
+require('onedark').setup  {
+    -- Change code style ---
+    -- Options are italic, bold, underline, none
+    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+    code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'bold'
+    },
+}
+
+require('onedark').load()
+
